2d4d899a2af102ce00f0204a5f6b5dd11b680cf1
andrewmudge <mudge.andrew@gmail.com>
2025-07-15 00:11:43 +0300

    Add amplify.yml configuration and environment variable fixes for server-side functions

--- next.config.js
+++ next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
     unoptimized: true,
     domains: ['images.pexels.com', 'd2ly4zque2pcbe.cloudfront.net']
   },
+  // Ensure environment variables are available to server-side functions
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || process.env.AUTH_SECRET,
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+    ADMIN_PASSWORD_HASH: process.env.ADMIN_PASSWORD_HASH,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 module.exports = nextConfig;
